adb3b31e515bcd32692177f4e30e9d0e62835df0
THAMMINAINARAJESH <rajeshthamminaina@gmail.com>
2025-06-19 12:31:57 +0530

    add 023_77_array_(Vishnu_Chakra).py

---
+++ 023_77_array_(Vishnu_Chakra).py
@@ -0,0 +1,8 @@
+n = int(input("enter some number: "))
+for i in range (1 , n+1):
+    for j in range (i , n):
+        if n == 0:
+            value = 1
+        elif n * (n-1):
+            j = n
+    print(j, end = " ")
